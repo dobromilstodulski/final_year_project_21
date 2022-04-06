b95a9cd0bef88a91a7036fc99e79747852bc07e3
DOBBOOK/bb100 <bb100dulski@gmail.com>
2022-04-06 09:50:55 +0100

    added custom error pages

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 import pusher
 import os
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from peewee import *
 from flask_login import LoginManager
@@ -20,6 +20,20 @@ pusher_client = pusher.Pusher(
     ssl=True
 )
 
+def bad_request(e):
+    return render_template('/error/400.html'), 400
+
+def unauthorized(e):
+    return render_template('/error/401.html'), 401
+
+def forbidden(e):
+    return render_template('/error/403.html'), 403
+
+def page_not_found(e):
+    return render_template('/error/404.html'), 404
+
+def internal_server_error(e):
+    return render_template('/error/500.html'), 500
 
 def create_app():
     app = Flask(__name__)
@@ -54,6 +68,12 @@ def create_app():
 
         from app.views import views
         app.register_blueprint(views)
+        
+        app.register_error_handler(400, bad_request)
+        app.register_error_handler(401, unauthorized)
+        app.register_error_handler(403, forbidden)
+        app.register_error_handler(404, page_not_found)
+        app.register_error_handler(404, internal_server_error)
 
         db.create_all()
 
