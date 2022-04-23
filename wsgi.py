b5f29b29379fe35ecac348b2647b9c535520a4e0
DOBBOOK/bb100 <bb100dulski@gmail.com>
2022-04-23 12:59:05 +0100

    updated socketio

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,10 @@
-from app import create_app
+from app import create_app, socketio
+from app.models import initialize
 
 if __name__ == '__main__':
     app = create_app()
-    app.run()
+    initialize()
+    socketio.run(app, host='0.0.0.0', port=5004)
 
 else:
     gunicorn = create_app()
